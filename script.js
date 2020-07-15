fdde0d10c0576eb5b4c66cb095593c2e2c8e7bac
Michael Vance <michaelvance0416@gmail.com>
2020-07-15 11:02:01 -0700

    finished mvp that takes a search query returns dynamic cards with job search results images and job titles. minimal css styling for card responsiveness

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ let locationInput = document.getElementById("location-input");
 let descriptionCheck = document.getElementById("description-check");
 let locationCheck = document.getElementById("location-check");
 let searchButton = document.getElementById("search-button");
+let outputContainer = document.getElementById("output-container");
 
-searchButton.addEventListener("click", someFunction);
+searchButton.addEventListener("click", createApiQuery);
 // Gather input
 
 // Produce output
-function someFunction(event) {
+function createApiQuery(event) {
   event.preventDefault();
   // example api call https://jobs.github.com/positions.json?description=python&full_time=true&location=sf
-  let apiQuery = "https://jobs.github.com/positions.json?";
+  let proxy = "https://cors-anywhere.herokuapp.com/";
+
+  let apiQuery = proxy + "https://jobs.github.com/positions.json?";
 
   // perform description check
   if (descriptionCheck.checked) {
@@ -35,4 +38,51 @@ function someFunction(event) {
   }
   // add location to api query
   console.log(apiQuery);
+
+  //Perform fetch and createCard as a call back function
+  fetch(apiQuery)
+    .then((response) => response.json())
+
+    .then((data) => createCards(data));
+}
+
+function createCards(data) {
+  console.log(data);
+  for (let i = 0; i < data.length; i++) {
+    let imgURL = data[i]["company_logo"];
+    let titleText = data[i]["title"];
+
+    let outputContainer = document.getElementById("output-container");
+
+    let card = document.createElement("div");
+    card.setAttribute("class", "card");
+    outputContainer.appendChild(card);
+
+    let image = document.createElement("img");
+    image.setAttribute("class", "card-img-top");
+    image.setAttribute("src", imgURL);
+    image.setAttribute("alt", "company-logo");
+    card.appendChild(image);
+
+    let cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+    card.appendChild(cardBody);
+
+    let cardTitle = document.createElement("h5");
+    cardTitle.setAttribute("class", "card-title");
+    cardTitle.textContent = titleText;
+    cardBody.appendChild(cardTitle);
+
+    let cardText1 = document.createElement("p");
+    cardText1.setAttribute("class", "card-text");
+    cardBody.appendChild(cardText1);
+
+    let cardText2 = document.createElement("p");
+    cardText2.setAttribute("class", "card-text");
+
+    cardBody.appendChild(cardText2);
+    let button = document.createElement("button");
+    button.setAttribute("class", "btn btn-primary");
+    cardBody.appendChild(button);
+  }
 }
